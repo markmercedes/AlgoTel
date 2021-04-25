5d1e9ba6609c38a49ff0cf440192bf5047c17c03
Marcos Mercedes <marcos.mercedesn@gmail.com>
2021-04-25 16:48:59 -0400

    Split frontend layout

---
+++ src/Views/Layout/frontend/header_menu.php
@@ -0,0 +1,64 @@
+<header class="header-section">
+  <div class="top-nav">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-6">
+          <ul class="tn-left">
+            <li><i class="fa fa-phone"></i> (12) 345 67890</li>
+            <li><i class="fa fa-envelope"></i> info.colorlib@gmail.com</li>
+          </ul>
+        </div>
+        <div class="col-lg-6">
+          <div class="tn-right">
+            <div class="top-social">
+              <a href="#"><i class="fab fa-facebook"></i></a>
+              <a href="#"><i class="fab fa-twitter"></i></a>
+              <a href="#"><i class="fab fa-tripadvisor"></i></a>
+              <a href="#"><i class="fab fa-instagram"></i></a>
+            </div>
+            <a href="#" class="bk-btn">Reserva ahora!</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="menu-item">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-2">
+          <div class="logo">
+            <h2 class="logo-title">
+              <a href="./index.html">
+                Eden Roc
+              </a>
+            </h2>
+          </div>
+        </div>
+        <div class="col-lg-10">
+          <div class="nav-menu">
+            <nav class="mainmenu">
+              <ul>
+                <li class="active"><a href="./index.html">Home</a></li>
+                <li><a href="./rooms.html">Rooms</a></li>
+                <li><a href="./about-us.html">About Us</a></li>
+                <li><a href="./pages.html">Pages</a>
+                  <ul class="dropdown">
+                    <li><a href="./room-details.html">Room Details</a></li>
+                    <li><a href="./blog-details.html">Blog Details</a></li>
+                    <li><a href="#">Family Room</a></li>
+                    <li><a href="#">Premium Room</a></li>
+                  </ul>
+                </li>
+                <li><a href="./blog.html">News</a></li>
+                <li><a href="./contact.html">Contact</a></li>
+              </ul>
+            </nav>
+            <div class="nav-right search-switch">
+              <i class="icon_search"></i>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</header>
